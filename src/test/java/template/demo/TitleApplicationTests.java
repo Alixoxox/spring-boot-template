f37c7b9eae175278483fb38e10d34b15c8815fe9
Alixoxox <Alixoxox@users.noreply.github.com>
2025-09-15 21:35:38 +0500

    initial state

---
+++ src/test/java/template/demo/TitleApplicationTests.java
@@ -0,0 +1,13 @@
+package template.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
